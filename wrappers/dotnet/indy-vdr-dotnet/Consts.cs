e839261fa628ed263586d32b3b7be7e4e3406184
SMichel-esatus <s.michel@esatus.com>
2023-04-27 13:00:14 +0200

    add xamarin ios to target

--- wrappers/dotnet/indy-vdr-dotnet/Consts.cs
+++ wrappers/dotnet/indy-vdr-dotnet/Consts.cs
@@ -2,6 +2,10 @@
 {
     public class Consts
     {
+#if __IOS__
+        public const string LIBINDY_VDR_NAME = "__Internal";
+#else
         public const string LIBINDY_VDR_NAME = "indy_vdr";
+#endif
     }
 }
